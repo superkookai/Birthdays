dab10b7c3a6a7b38ac429bf42eafedac55e4d0bc
superkookai <weerawut@live.com>
2025-05-17 14:39:42 +0700

    Done

---
+++ Birthdays/Friend.swift
@@ -0,0 +1,24 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Weerawut Chaiyasomboon on 17/05/2568.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+    
+    var isBirthdayToday: Bool {
+        Calendar.current.isDateInToday(birthday)
+    }
+}
